69c7b9e0003a04c385a136f8d14fc72de9000abf
Drazon <jdvu21@gmail.com>
2016-03-31 21:59:46 -0400

    implemented masterCode generator, guess countDown, win condition

--- mastermind.cs
+++ mastermind.cs
@@ -1,19 +1,75 @@
+/* 
+   This is a C# program that can be compiled using:
+   csc (windows, c sharp compiler)
+   msc (OSX, mono compiler)
+*/
+
+
 using System;
 
 class Mastermind
 {
     static void Main (string[] args)
     {
-    	int chances = 0;
+    	int chances;
+    	int len = 4;
+        string masterCode = "";
 
-    	if (args[0] == null) 
+    	if (args.Length == 0) 
     	{
     		Console.WriteLine("You did not declare number of trys. Please do so now:");
-    		Console.ReadLine();
+    		chances = Convert.ToInt16(Console.ReadLine());
     	}
     	else
-    		chances = Convert.ToInt32(args[0]);
-    		
-    	Console.WriteLine("You have {0} attempts", chances);
-    }
+    		chances = Convert.ToInt16(args[0]);
+
+    	Random roll = new Random();
+    	for (int i = 0; i < len; ++i)
+    	{
+            string t = Convert.ToString(roll.Next(1,7));
+            masterCode += t;
+    	}
+
+    	// for debug
+        // Console.WriteLine("Master code has been generated.");
+        Console.WriteLine("{0}", masterCode);
+
+    while (chances != 0) {
+        Console.WriteLine("You have {0} guesses remaining.", chances);
+        Console.WriteLine("Please enter your guess:");
+        string currentGuess = Convert.ToString(Console.ReadLine());
+        int correctGuess = 0;
+        int almostRight = 0;
+
+        if (masterCode[0] == currentGuess[0] &&
+            masterCode[1] == currentGuess[1] &&
+            masterCode[2] == currentGuess[2] &&
+            masterCode[3] == currentGuess[3]) {
+            Console.WriteLine("You solved it!");
+            System.Environment.Exit(1); 
+        }
+
+        else {
+            for (int i = 0; i < currentGuess.Length; ++i) {
+
+            for (int j = 0; j < masterCode.Length; ++j) {
+                if (masterCode[i] == currentGuess[j]) {
+                    ++correctGuess;
+                    }
+                }
+            }
+
+            for (int i = 0; i < correctGuess; ++i) {
+                Console.WriteLine("+");
+            }
+            for (int i = 0; i < almostRight; ++i) {
+                Console.WriteLine("-");
+            }
+            
+            chances--;
+            }
+        }
+        Console.WriteLine("You lose :(");
+        Console.WriteLine("The mastercode was: {0}", masterCode);
+    }    
 }
